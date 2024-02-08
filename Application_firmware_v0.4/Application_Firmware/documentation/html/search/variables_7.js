5ce36cb9ba444a9301fc47eea99a47685b76781e
Mortrack <cmirandameza3@hotmail.com>
2024-02-07 20:06:43 -0800

    I have updated the Doxygen Offline documentation with respect to the latest changes made from my latest commit.

--- Application_firmware_v0.4/Application_Firmware/documentation/html/search/variables_7.js
+++ Application_firmware_v0.4/Application_Firmware/documentation/html/search/variables_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['gpio_5fpin_0',['GPIO_Pin',['../dd/db4/struct_h_m10___g_p_i_o__def__t.html#ad66a323e02c0cdfeefb9a2a766b1f40e',1,'HM10_GPIO_def_t']]],
-  ['gpio_5fport_1',['GPIO_Port',['../dd/db4/struct_h_m10___g_p_i_o__def__t.html#a02fb039f1080c85c379d3dbaa59e804b',1,'HM10_GPIO_def_t']]]
+  ['gpio_5fis_5fhm10_5fdefault_5fsettings_0',['GPIO_is_hm10_default_settings',['../d3/dab/group__main.html#ga078a3abaad09cd25cc49921bb926e4ab',1,'main.c']]],
+  ['gpio_5fpin_1',['GPIO_Pin',['../dd/db4/struct_h_m10___g_p_i_o__def__t.html#ad66a323e02c0cdfeefb9a2a766b1f40e',1,'HM10_GPIO_def_t']]],
+  ['gpio_5fport_2',['GPIO_Port',['../dd/db4/struct_h_m10___g_p_i_o__def__t.html#a02fb039f1080c85c379d3dbaa59e804b',1,'HM10_GPIO_def_t']]]
 ];
--- Custom_Bootloader_v0.4/Custom_Bootloader_Firmware/documentation/html/search/all_7.js
+++ Custom_Bootloader_v0.4/Custom_Bootloader_Firmware/documentation/html/search/all_7.js
@@ -9,9 +9,10 @@ var searchData=
   ['get_5fhm10_5fpin_5fcode_5fmode_6',['get_hm10_pin_code_mode',['../dd/d6e/group__hm10__ble.html#ga27b7aa6af1b8b4b12248c2167679f095',1,'hm10_ble_driver.c']]],
   ['get_5fhm10_5frole_7',['get_hm10_role',['../dd/d6e/group__hm10__ble.html#ga554fd3ad2efe6e24c1ed4b66d85895c1',1,'hm10_ble_driver.c']]],
   ['goto_5fapplication_5ffirmware_8',['goto_application_firmware',['../d3/dab/group__main.html#gacafedc0813afcec11b7ead2ffddcaf64',1,'main.c']]],
-  ['gpio_5fis_5fhm10_5fdefault_5fsettings_5fpin_9',['GPIO_is_hm10_default_settings_Pin',['../d3/dab/group__main.html#ga7a929513ed85bf8b6a636218f5e0fade',1,'main.c']]],
-  ['gpio_5fis_5fhm10_5fdefault_5fsettings_5fport_10',['GPIO_is_hm10_default_settings_Port',['../d3/dab/group__main.html#gae56ed3f67cf0aa7b36af259ba6d59a64',1,'main.c']]],
-  ['gpio_5fmcu_5fled1_5fpin_11',['GPIO_MCU_LED1_Pin',['../d3/dab/group__main.html#ga5790efbdeeb252c0c2eea75abcedfadb',1,'main.c']]],
-  ['gpio_5fpin_12',['GPIO_Pin',['../dd/db4/struct_h_m10___g_p_i_o__def__t.html#ad66a323e02c0cdfeefb9a2a766b1f40e',1,'HM10_GPIO_def_t']]],
-  ['gpio_5fport_13',['GPIO_Port',['../dd/db4/struct_h_m10___g_p_i_o__def__t.html#a02fb039f1080c85c379d3dbaa59e804b',1,'HM10_GPIO_def_t']]]
+  ['gpio_5fis_5fhm10_5fdefault_5fsettings_9',['GPIO_is_hm10_default_settings',['../d3/dab/group__main.html#ga078a3abaad09cd25cc49921bb926e4ab',1,'main.c']]],
+  ['gpio_5fis_5fhm10_5fdefault_5fsettings_5fpin_10',['GPIO_is_hm10_default_settings_Pin',['../d3/dab/group__main.html#ga7a929513ed85bf8b6a636218f5e0fade',1,'main.c']]],
+  ['gpio_5fis_5fhm10_5fdefault_5fsettings_5fport_11',['GPIO_is_hm10_default_settings_Port',['../d3/dab/group__main.html#gae56ed3f67cf0aa7b36af259ba6d59a64',1,'main.c']]],
+  ['gpio_5fmcu_5fled1_5fpin_12',['GPIO_MCU_LED1_Pin',['../d3/dab/group__main.html#ga5790efbdeeb252c0c2eea75abcedfadb',1,'main.c']]],
+  ['gpio_5fpin_13',['GPIO_Pin',['../dd/db4/struct_h_m10___g_p_i_o__def__t.html#ad66a323e02c0cdfeefb9a2a766b1f40e',1,'HM10_GPIO_def_t']]],
+  ['gpio_5fport_14',['GPIO_Port',['../dd/db4/struct_h_m10___g_p_i_o__def__t.html#a02fb039f1080c85c379d3dbaa59e804b',1,'HM10_GPIO_def_t']]]
 ];
